e49af1a06c309afd1028b38af3c60b29c9c52fa7
wupeng <710186001@qq.com>
2017-11-03 18:32:54 +0800

    ref and transition

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,36 +1,35 @@
 <template>
   <div>
-    <!-- <div class="transition">
-      <transition name="move">
-        <div class="decrease" v-show="count>0" @click="decrease">
-          <i class="fa fa-minus-circle" aria-hidden="true"></i>
+    <!-- 用在单个元素上 -->
+    <div class="single-content">
+      <button @click="single = !single">button</button>
+      <transition name="single">
+        <div v-show="single" class="single">hello</div>
+      </transition>
+    </div>
+    <!-- 滚动的小球 -->
+    <div class="rotate-content">
+      <button @click="rotateBall = !rotateBall">button</button>
+      <transition name="rotate">
+        <div class="ball" v-show="rotateBall">
+          <div class="inner"></div>
         </div>
       </transition>
-      <div class="count" v-show="count>0">{{count}}</div>
-      <div class="add" @click="add" ref="test">
-        <i class="fa fa-plus-circle" aria-hidden="true"></i>
-      </div>
-    </div>  -->
+    </div>
+    <!-- 1px 边框 -->
+    <div class="container border-1px"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
   export default {
+    components: {
+
+    },
     data () {
       return {
-        count: 0
-      }
-    },
-    methods: {
-      add () {
-        console.log(this.$refs.test)
-        this.count++
-      },
-      decrease () {
-        if (this.count === 0) {
-          return
-        }
-        this.count--
+        single: false,
+        rotateBall: true
       }
     }
   }
@@ -38,35 +37,50 @@
 </script>
 
 <style lang="scss" rel="stylesheet/scss" scoped>
-  .transition{
-    margin-top: 30px;
-    font-size: 0;
-    div{
-      display: inline-block;
-      padding: 6px;
-      font-size: 24px;
-      line-height: 24px;
-      color: #00A0DC;
-    }
-    .decrease{
-      transition: all 0.6s linear;
-      .fa{
-         display: inline-block;
-            transition: all 0.4s linear;
-            transform: rotate(0);
-      }
-      &.move-enter-active{
-            opacity: 1;
-            transform: translate3d(0,0,0);
-        }
-        &.move-enter, &.move-leave-active{
-            opacity: 0;
-            transform: translate3d(24px, 0, 0);
-            .fa{
-                opacity: 0;
-                transform: rotate(180deg);
-            }
-        }
+@import "../../common/scss/mixin";
+div[class$="-content"]{
+  margin: 15px;
+  padding: 15px;
+  border: 3px double #ddd;
+}
+.single{
+  &.single-enter-active, &.single-leave-active{
+    transition: opacity 3s;
+  }
+  &.single-enter, &.single-leave-to{
+    opacity: 0;
+  }
+}
+
+
+.ball{
+  margin-top: 15px;
+  transform: translate3d(0,0,0);
+  .inner{
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    background-color: rgb(0,160,220);
+    border-radius: 50%;
+    transition: all 0.6s linear;
+    transform: rotate(0);
+  }
+  &.rotate-enter-active, &.rotate-leave-active{
+    transition: all 0.4s linear;
+  }
+  &.rotate-enter, &.rotate-leave-to{
+    opacity: 0;
+    transform: translate3d(24px, 0, 0);
+    .inner{
+        transform: rotate(180deg);
     }
   }
+}
+
+.container{
+  width: 100px;
+  height: 100px;
+  margin: 20px auto;
+  @include border-1px(red);
+}
 </style>
