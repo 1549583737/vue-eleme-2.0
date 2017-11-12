e6da36d9a434e130a5911a38a6f58fe7dd16b70c
frwupeng517 <710186001@qq.com>
2017-11-12 23:03:25 +0800

    ratings page

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,44 +1,47 @@
 <template>
-  <div>
-    <!-- 用在单个元素上 -->
-    <div class="single-content">
-      <button @click="single = !single">button</button>
-      <transition name="single">
-        <div v-show="single" class="single">hello</div>
-      </transition>
-    </div>
-    <!-- 滚动的小球 -->
-    <div class="rotate-content">
-      <button @click="rotateBall = !rotateBall">button</button>
-      <transition name="rotate">
-        <div class="ball" v-show="rotateBall"> <!-- 外层控制位移 -->
-          <div class="inner"></div> <!-- 内层控制旋转 -->
+  <div class="ratings">
+    <div class="ratings-content">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">{{seller.score}}</h1>
+          <h2 class="title">综合评分</h2>
+          <h3 class="rank">高于周边商家{{seller.rankRate}}%</h3>
         </div>
-      </transition>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <star :size="36" :score="seller.serviceScore"></star>
+            <span class="score">{{seller.serviceScore}}</span>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <star :size="36" :score="seller.foodScore"></star>
+            <span class="score">{{seller.foodScore}}</span>
+          </div>
+          <div class="delivery-wrapper">
+            <span class="title">送达时间</span>
+            <span class="delivery">{{seller.deliveryTime}}分钟</span>
+          </div>
+        </div>
+      </div>
     </div>
-    <!-- 1px 边框 -->
-    <div class="container border-1px"></div>
-    <!-- Range.getBoundingClientRect() https://developer.mozilla.org/zh-CN/docs/Web/API/Element/getBoundingClientRect -->
-    <div class="bounding bounding-hook" style="width: 100px; height: 100px; margin-left: 15px; background-color: rgb(0,160,220)"></div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+  import star from '../star/star'
   export default {
+    props: {
+      seller: {
+        type: Object
+      }
+    },
     components: {
-
+      star
     },
     data () {
       return {
-        single: false,
-        rotateBall: true
       }
-    },
-    created () {
-      this.$nextTick(() => {
-        let rect = document.getElementsByClassName('bounding-hook')[0].getBoundingClientRect()
-        console.log(rect)
-      })
     }
   }
 
@@ -46,49 +49,83 @@
 
 <style lang="scss" rel="stylesheet/scss" scoped>
 @import "../../common/scss/mixin";
-div[class$="-content"]{
-  margin: 15px;
-  padding: 15px;
-  border: 3px double #ddd;
-}
-.single{
-  &.single-enter-active, &.single-leave-active{
-    transition: opacity 3s;
-  }
-  &.single-enter, &.single-leave-to{
-    opacity: 0;
-  }
-}
+.ratings{
+  position: absolute;
+  left: 0;
+  top: 178px;
+  bottom: 0;
+  overflow: hidden;
+  .overview{
+    display: flex;
+    padding: 18px 0;
+    .overview-left{
+      flex: 0 0 137px;
+      width: 137px;
+      padding: 6px 0;
+      text-align: center;
+      border-right: 1px solid rgba(7,17,27,0.1);
+      .score{
+        line-height: 28px;
+        font-size: 24px;
+        color: rgb(255, 153, 0);
+      }
+      .title{
+        margin: 6px 0 8px 0;
+        line-height: 12px;
+        font-size: 12px;
+        color: rgb(7,17,27);
+      }
+      .rank{
+        line-height: 10px;
+        font-size: 10px;
+        color: rgb(147,153,159);
+      }
+    }
+    .overview-right{
+      flex: 1;
+      padding: 0 24px;
+      .score-wrapper{
+        margin-bottom: 8px;
+        font-size: 0;
+        .title{
+          display: inline-block;
+          vertical-align: top;
+          line-height: 18px;
+          font-size: 12px;
+          color: rgb(7,17,27);
+        }
+        .star{
+          display: inline-block;
+          margin: 0 12px;
 
-
-.ball{
-  margin-top: 15px;
-  transform: translate3d(0,0,0);
-  .inner{
-    display: inline-block;
-    width: 16px;
-    height: 16px;
-    background-color: rgb(0,160,220);
-    border-radius: 50%;
-    transition: all 0.6s linear;
-    transform: rotate(0);
-  }
-  &.rotate-enter-active, &.rotate-leave-active{
-    transition: all 0.4s linear;
-  }
-  &.rotate-enter, &.rotate-leave-to{
-    opacity: 0;
-    transform: translate3d(24px, 0, 0);
-    .inner{
-        transform: rotate(180deg);
+        }
+        .score{
+          display: inline-block;
+          vertical-align: top;
+          line-height: 18px;
+          font-size: 12px;
+          color: rgb(255, 153, 0);
+        }
+      }
+      .delivery-wrapper{
+        font-size: 0;
+        .title{
+          display: inline-block;
+          margin-right: 12px;
+          vertical-align: top;
+          line-height: 18px;
+          font-size: 12px;
+          color: rgb(7,17,27);
+        }
+        .delivery{
+          display: inline-block;
+          vertical-align: top;
+          font-size: 12px;
+          line-height: 18px;
+          color: rgb(147,153,159);
+        }
+      }
     }
   }
-}
-
-.container{
-  width: 100px;
-  height: 100px;
-  margin: 20px auto;
-  @include border-1px(red);
 }
 </style>
